79f2f93cce81ea852891f58835cc9020e1d796b7
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-02-14 19:03:59 +0530

    second

--- src/main/java/com/driver/MovieService.java
+++ src/main/java/com/driver/MovieService.java
@@ -1,14 +1,8 @@
 package com.driver;
 
-import com.driver.model.Director;
-import com.driver.model.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
